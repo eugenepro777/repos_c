c6ceec36161d328c2ced7ed4848ef402d180bece
Eugene_Pro <proskuryakov.e@list.ru>
2022-11-09 20:36:34 +0300

    add Ex003 Summ project

--- Ex003/Program.cs
+++ Ex003/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("2610 2022   Hello, World!");
+﻿int numberA = 31;
+int numberB = 5;
+int result = numberA + numberB;
+Console.WriteLine(result);
